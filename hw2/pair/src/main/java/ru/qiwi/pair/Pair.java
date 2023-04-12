b8ba273a96daa399656a7b273d67521eb7a13c71
Toma <t.troshkina@qiwi.com>
2023-04-12 11:50:33 +0300

    add pair task

--- hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
+++ hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
@@ -1,5 +1,27 @@
 package ru.qiwi.pair;
 
-public class Pair<One, Two> {
+public final class Pair<One, Two> {
+    private final One first;
+    private final Two second;
 
+    private Pair(One first, Two second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <One, Two> Pair<One, Two> of(One first, Two second) {
+        return new Pair<>(first, second);
+    }
+
+    public One getFirst() {
+        return first;
+    }
+
+    public Two getSecond() {
+        return second;
+    }
+
+    public boolean equals(Pair<One, Two> pair) {
+        return this.first == pair.getFirst() && this.second == pair.getSecond();
+    }
 }
